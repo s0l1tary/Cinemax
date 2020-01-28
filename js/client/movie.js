8c6d3ff2a8b71992886520892b3e781e28fb5069
unknown <theinfiniteboxx@gmail.com>
2020-01-28 18:09:50 +0800

    movies updated

--- js/client/movie.js
+++ js/client/movie.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
     var URLParams = new URLSearchParams(window.location.search);
-    var movieID = URLParams.get('id');
+    var pageNo = URLParams.get('page');
+
+    $.ajax({
+        url: "/api/movies/topRated/"+pageNo,
+        method: "get"
+    }).done (
+        function(data) {
+            var content = "<div>";
+            content.append()
+        }
+    )
 });
