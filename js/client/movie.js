10d9d3a3d8088263d4f61c46c964e64dc2d90c80
unknown <theinfiniteboxx@gmail.com>
2020-01-29 17:58:58 +0800

    indexpage

--- js/client/movie.js
+++ js/client/movie.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
     var URLParams = new URLSearchParams(window.location.search);
     var pageNo = URLParams.get('page');
-
-    $.ajax({
-        url: "/api/movies/topRated/"+pageNo,
-        method: "get"
-    }).done (
-        function(data) {
-            var content = "<div>";
-            content.append()
-        }
-    )
 });
