3827bdae67e15860503c0eba21fb24c5aa05c942
unknown <theinfiniteboxx@gmail.com>
2020-01-31 15:34:46 +0800

    cinemas & timeslots

--- js/client/showtimes.js
+++ js/client/showtimes.js
@@ -1,30 +1,18 @@
 $(document).ready(function () {
     var URLParams = new URLSearchParams(window.location.search);
     var movieId = URLParams.get('id');
+
     $.ajax({
         url: "/api/cinemas",
         method: "get",
         dataType: "json"
     }).done(
         function(data) {
-            $.each(data), function(key,value) {
-                $('.cinemalist').append("<li>"+value.name+"</li>");
-            }
-
+            $.each(data, function(key,value) {
+                $('.cinemalist').append("<li><button onclick=\"$.ajax({url: '/api/showings/'+"+movieId+",method: 'get',dataType: 'json'}).done(function(data){$('.timeslotlist').empty();$.each(data,function(key,value){if(value.cinemaId=='"+value._id+"'){$('.timeslotlist').append(value.timing.Time)}})}).fail(function(err) {});\" id="+value._id+">"+value.name+ " " +value.branch+"</button></li>");
+            });
     }).fail(
         function(err) {
-
-    });
-
-    $.ajax({
-        url: "/api/showings/"+movieId,
-        method: "get",
-        dataType: "json"
-    }).done(
-        function(data) {
-
-    }).fail(
-        function(err) {
-
-    });
-});
+        
+        })
+})
